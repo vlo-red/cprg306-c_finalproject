c3db56be4917885a3cdc9a6ceff7dbb24db44bb8
vlo-red <jared.lopez@edu.sait.ca>
2026-04-16 16:48:07 -0600

    Configured firebase.js and added pages for web app

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase config
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,6 +13,9 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
+// Initialize app
 const app = initializeApp(firebaseConfig);
+
+// Services
 export const auth = getAuth(app);
+export const db = getFirestore(app);
